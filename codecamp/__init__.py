2209deafb6a0039cff3d0fd323a88bdf24b43c14
Lara Schmidt <lara.schmidt205@gmail.com>
2025-02-25 14:42:59 +0100

    Part 3 done

--- codecamp/__init__.py
+++ codecamp/__init__.py
@@ -1,6 +1,7 @@
 """Turbie functions.
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_resp(path_resp, t_start = 60):
     """An example function in a package."""
@@ -37,3 +38,37 @@ def load_wind(path_resp, t_start = 0):
     u_wind = data[i:, 1]  # Second column: V(m/s)
 
     return t_wind, u_wind
+
+def plot_resp(t, u, xb, xt):
+    
+    # Create subplots with improved spacing
+    fig, axs = plt.subplots(3, 1, figsize=(9, 4))
+
+    # First plot
+    axs[0].plot(t, u, color='tab:blue', linewidth=0.5)
+    axs[0].set_title('U over Time', fontsize=12, fontweight='bold')
+    axs[0].set_xlabel('$t$ in [s]', fontsize=10)
+    axs[0].set_ylabel('$u(t)$ in [m/s]', fontsize=10)
+    axs[0].grid(True, linestyle='--', alpha=0.6)
+    axs[0].set_xlim(60,600)
+
+    # Second plot
+    axs[1].plot(t, xb, color='tab:blue', linewidth=0.5)
+    axs[1].set_title('Xb over Time', fontsize=12, fontweight='bold')
+    axs[1].set_xlabel('$t$ in [s]', fontsize=10)
+    axs[1].set_ylabel('$xb(t)$ in [m]', fontsize=10)
+    axs[1].grid(True, linestyle='--', alpha=0.6)
+    axs[1].set_xlim(60,600)
+
+    # Third plot
+    axs[2].plot(t, xt, color='tab:blue', linewidth=0.5)
+    axs[2].set_title('Xt over Time', fontsize=12, fontweight='bold')
+    axs[2].set_xlabel('$t$ in [s]', fontsize=10)
+    axs[2].set_ylabel('$xt(t)$ in [m]', fontsize=10)
+    axs[2].grid(True, linestyle='--', alpha=0.6)
+    axs[2].set_xlim(60,600)
+
+    # Show the plot
+    fig.tight_layout()
+    plt.show()
+    
