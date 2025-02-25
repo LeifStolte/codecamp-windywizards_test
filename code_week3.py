7be709f66b95b5ecbdfde0ce2b679e38bf716534
Leif Stolte <s250243@dtu.dk>
2025-02-25 13:25:12 +0100

    Part 2 done

--- code_week3.py
+++ code_week3.py
@@ -1,8 +1,25 @@
 """Script for the Week 3 assignment."""
 import codecamp
+import os
+
+# Get the absolute path of the parent directory
+parent_dir = os.path.dirname(__file__)
+
+filename_resp = parent_dir + r"\data\resp_12_ms_TI_0.1.txt"
+filename_wind = parent_dir + r"\data\wind_12_ms_TI_0.1.txt"
+
+# Call load_resp with the correct relative path
 
 
-# example of how you can call a function you place in codecamp/__init__.py
-codecamp.example()
+# Input ______________________________________________
 
-# TODO! Delete the line above and add your code to solve the weekly assignment.
+# in s
+t_resp_start = 60 
+t_wind_start = 0 
+
+# Calc _________________________________________________
+
+codecamp.load_resp(filename_resp,t_resp_start)
+
+
+codecamp.load_wind(filename_wind,t_wind_start)
--- codecamp/__init__.py
+++ codecamp/__init__.py
@@ -2,7 +2,7 @@
 """
 import numpy as np
 
-def load_resp(path_resp, t_start = 0):
+def load_resp(path_resp, t_start = 60):
     """An example function in a package."""
     # Load the data, skipping the header row
     data = np.loadtxt(path_resp, skiprows=1)
@@ -15,14 +15,18 @@ def load_resp(path_resp, t_start = 0):
     u = data[i:, 1]  # Second column: V(m/s)
     xb = data[i:, 2]  # Third column: xb(m)
     xt = data[i:, 3]  # Fourth column: xt(m)
-
-    # Print first few values for verification
-    print("Time:", t[:5])
-    print("Velocity:", u[:5])
-    print("xb:", xb[:5])
-    print("xt:", xt[:5])
-
     return t, u, xb, xt
 
-load_resp(r"C:\Users\fhuer\OneDrive\Dokumente\Lara\GitHub\codecamp-windywizards\data\resp_12_ms_TI_0.1.txt",60)
+def load_wind(path_resp, t_start = 0):
+    """An example function in a package."""
+    # Load the data, skipping the header row
+    data = np.loadtxt(path_resp, skiprows=1)
+    #create a start and end value for loading our data
+    i = 0
+    while data[i,0] < t_start:
+        i += 1
+    # Extract columns into separate NumPy arrays
+    t_wind = data[i:, 0]  # First column: Time(s)
+    u_wind = data[i:, 1]  # Second column: V(m/s)
 
+    return t_wind, u_wind
